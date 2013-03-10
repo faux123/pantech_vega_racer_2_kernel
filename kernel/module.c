0212445df710ad281fcd3899030f32cd19e5b376
AlexBokhankovich <alex.bokhankovich@gmail.com>
2013-03-11 02:04:40 +0400

    HACK: allow loading precompiled Wlan module

--- kernel/module.c
+++ kernel/module.c
@@ -2433,7 +2433,8 @@ static int check_modinfo(struct module *mod, struct load_info *info)
 	} else if (!same_magic(modmagic, vermagic, info->index.vers)) {
 		printk(KERN_ERR "%s: version magic '%s' should be '%s'\n",
 		       mod->name, modmagic, vermagic);
-		return -ENOEXEC;
+		/* This is temporary workaround to make it possible to load precompiled kernel module */
+		/* return -ENOEXEC; */
 	}
 
 	if (get_modinfo(info, "staging")) {
